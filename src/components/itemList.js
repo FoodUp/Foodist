41eaaa222d97e00cc539e45244d81ca6d87a3bb8
coucou_miya <ybb.emily@gmail.com>
2018-08-23 08:18:11 +0200

    add icon

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { r , card , itemImg, title, time,  shadow, imgWrapper } from '../style/components/itemList.css';
-
+import { basket } from '../style/shared/icon.css';
 const ItemList = (props) =>{
     const itemsList =  _.map(props.items, (item, id)=> {
         var itemStyle = {
@@ -16,7 +16,11 @@ const ItemList = (props) =>{
                     <div className={ shadow }></div>
                 </div>
                 <div className ={ title }>{item.name}</div>
-                <div className = { time }>{ item.time.amount } { item.time.unit }</div>              
+                <div className ={ basket }></div>           
+                
+                <div className = { time }>
+                { item.time.amount } { item.time.unit }
+                </div>   
             </div>
         );
     });
