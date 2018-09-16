29ba49d14f98800171710d65e264697e19bb0f76
coucou_miya <ybb.emily@gmail.com>
2018-09-17 01:06:57 +0200

    add new icons

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { img, intro, flex, steps, tag } from '../style/components/recipe.css';
+import { timer, pacman } from '../style/shared/icon.css';
 
 const Recipe = ({match, recipeObj}) => {
     const imageStyle = {
         backgroundImage : `url("/image/recipes/${recipeObj.image}")`,
-        backgroundRepeat : 'no-repeat',
-        height: '200px'
     }
     const tagList = recipeObj.tag.map((element)=>{
-        return (<span key= {element}>{ element }</span>);
+        return (<span className= {tag} key= {element}>{ element }</span>);
     });
     return (
-        <div>
-            <h2> { recipeObj.name }  </h2>
-            <p> { recipeObj.description }</p>
-            <div style = {imageStyle} ></div>
-            <div>
-                {tagList}
+        <div className ="container">
+            <div className={flex}>
+                <div className = { img } style = {imageStyle} ></div>
+                <div className = { intro }>
+                    <h2> { recipeObj.name }  </h2>
+                    <p> { recipeObj.description }</p>
+                    <div>
+                        <span className={ timer } ></span>
+                        {recipeObj.time.amount }  {recipeObj.time.unit} 
+                        <span className={ pacman }></span>  { recipeObj.person } persons 
+                    </div>
+                    <div>
+                        <span>{ recipeObj.type }</span>
+                        {tagList}
+                    </div>
+                </div>
             </div>
+            <div className ={ steps }>
+
+            </div>
+            
         </div>
        
     );
