703d7dd15061444248da60e7825cb9d9c2ca09d6
coucou_miya <ybb.emily@gmail.com>
2018-10-08 23:03:32 +0200

    update style

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from './searchBar';
+import { connect } from 'react-redux';
 
-import { nav, logo, chef, list, logoLink } from '../style/components/header.css';
+import { nav, logo, chef, basketIcon, basket, logoLink, quantity } from '../style/components/header.css';
 
-const NavBar = (props) => {
+const NavBar = ({ count }) => {
+    const displayBasket = (count>0)?
+                            (<div className={ basket }><div className={ basketIcon }></div><div className={ quantity }>{count}</div></div>)
+                            :(<div className={ basketIcon }></div>);
     return (
         <nav className={nav}>
             <Link to="/" className={ logoLink } >
                 <div className={`${logo}` }></div>
             </Link>
             <SearchBar />
-            <Link to="/shoppinglist" className={ list }></Link>
+            <Link to="/shoppinglist">
+                { displayBasket }
+            </Link>
+            
             <div className={ chef }></div>                    
         </nav>
     );
 }
 
-export default NavBar;
+const mapStateToProps = (state) =>{
+    return { count : state.addedRecipes.length }
+}
+export default connect(mapStateToProps)(NavBar);
