4e16b873001fa8a4c1d4eed7acb387433872c14f
coucou_miya <ybb.emily@gmail.com>
2018-10-14 21:21:57 +0200

    reformat

--- src/components/recipeItem.js
+++ src/components/recipeItem.js
@@ -1,54 +1,77 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import { addToBasket, removeBasket } from '../actions/index';
-import { card , title, time, basketContainer, addedBasketContainer,  shadow, imgWrapper } from '../style/components/itemList.css';
-import { basket } from '../style/shared/icon.css';
-import ProgressiveImage from './progressiveImage';
+import { addToBasket, removeBasket } from "../actions/index";
+import {
+  card,
+  title,
+  time,
+  basketContainer,
+  addedBasketContainer,
+  shadow,
+  imgWrapper
+} from "../style/components/itemList.css";
+import { basket } from "../style/shared/icon.css";
+import ProgressiveImage from "./progressiveImage";
 
-const RecipeItem = ({ inBasket, item , addToBasket, removeBasket }) =>{
-    return(
-        <div className={ card } > 
-            <Link to={`/recipe/${item.id}`} >  
-                <div className={ imgWrapper }>
-                    <ProgressiveImage image = {`/image/recipes/${item.image}`} color = {item.color}/>
-                    <div className={ shadow }></div>
-                </div>
-            </Link>
-            <div>
-                <div className ={ title }>{item.name}</div>        
-                <div className = { time }>{ item.time.amount } { item.time.unit }</div>   
-                <div className ={inBasket?addedBasketContainer:basketContainer }>
-                    <div className = { basket } onClick = {()=> inBasket?removeBasket(item.id):addToBasket(item.id) } ></div>
-                </div> 
-            </div>   
+const RecipeItem = ({ inBasket, item, addToBasket, removeBasket }) => {
+  return (
+    <div className={card}>
+      <Link to={`/recipe/${item.id}`}>
+        <div className={imgWrapper}>
+          <ProgressiveImage
+            image={`/image/recipes/${item.image}`}
+            color={item.color}
+          />
+          <div className={shadow} />
         </div>
-    )
-}
-const mapStateToProps= (state , { item })=>{
-    return {
-        inBasket : (state.addedRecipes.findIndex((e)=>{
-            return e.id === item.id
-        }))>-1 ? true: false
-    }
-
-}
+      </Link>
+      <div>
+        <div className={title}>{item.name}</div>
+        <div className={time}>
+          {item.time.amount} {item.time.unit}
+        </div>
+        <div className={inBasket ? addedBasketContainer : basketContainer}>
+          <div
+            className={basket}
+            onClick={() =>
+              inBasket ? removeBasket(item.id) : addToBasket(item.id)
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state, { item }) => {
+  return {
+    inBasket:
+      state.addedRecipes.findIndex(e => {
+        return e.id === item.id;
+      }) > -1
+        ? true
+        : false
+  };
+};
 RecipeItem.propTypes = {
-    item: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        image : PropTypes.string.isRequired,
-        color: PropTypes.string.isRequired,
-        name : PropTypes.string.isRequired,
-        time: PropTypes.shape({
-            amount: PropTypes.string.isRequired,
-            unit: PropTypes.string.isRequired
-        })
-    }),
-    addToBasket: PropTypes.func.isRequired,
-    removeBasket: PropTypes.func.isRequired,
-    inBasket : PropTypes.bool.isRequired
-}
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    time: PropTypes.shape({
+      amount: PropTypes.string.isRequired,
+      unit: PropTypes.string.isRequired
+    })
+  }),
+  addToBasket: PropTypes.func.isRequired,
+  removeBasket: PropTypes.func.isRequired,
+  inBasket: PropTypes.bool.isRequired
+};
 
-export default connect(mapStateToProps, { addToBasket, removeBasket })(RecipeItem);
+export default connect(
+  mapStateToProps,
+  { addToBasket, removeBasket }
+)(RecipeItem);
