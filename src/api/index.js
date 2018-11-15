bc8b580ab3a3750f808f126c9e28426183ac8b4a
coucou_miya <ybb.emily@gmail.com>
2018-11-15 08:44:51 +0100

    search item from server

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/recipes";
+const API_URL = "http://142.93.224.169:3000/recipes";
 
 export const fetchItems = () => {
   return fetch(API_URL, {
@@ -13,6 +13,15 @@ export const fetchItems = () => {
 
 export const fetchItemById = id => {
   return fetch(`${API_URL}/${id}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json"
+    }
+  }).then(res => res.json());
+};
+
+export const searchItem = term => {
+  return fetch(`${API_URL}/search?term=${term}`, {
     method: "GET",
     headers: {
       accept: "application/json"
