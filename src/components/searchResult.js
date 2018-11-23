1a34b8be5e82bfb8b49d372e098b32e9ce40c546
coucou_miya <ybb.emily@gmail.com>
2018-11-23 01:04:24 +0100

    fetch item when urlTerm change

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import ItemsList from "./itemsList";
 import { fetchItems } from "../actions";
-import { getSearchItems } from "../reducers";
+import { getSearchItems, getIsFetching } from "../reducers";
 
 class SearchResultPage extends Component {
   componentDidMount() {
     this.props.fetchItems(this.props.urlTerm);
   }
+  componentDidUpdate(prevProps) {
+    if (this.props.urlTerm !== prevProps.urlTerm) {
+      this.props.fetchItems(this.props.urlTerm);
+    }
+  }
   render() {
     return (
       <div className="container">
-        <ItemsList items={this.props.items} />
+        {this.props.isFetching ? (
+          <div>loading </div>
+        ) : (
+          <ItemsList items={this.props.items} />
+        )}
       </div>
     );
   }
@@ -20,6 +29,7 @@ class SearchResultPage extends Component {
 function mapStateToProps(state, ownProps) {
   //TODO: transfer url param to meaningful search term , like 'sea-food' to 'sea food'
   return {
+    isFetching: getIsFetching(state),
     urlTerm: ownProps.match.params.term,
     items: getSearchItems(state)
   };
